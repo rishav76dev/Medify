6f2b0d7730a63e07833689fe45fffeafd257f952
rishav76dev <rjrishav037@gmail.com>
2025-05-09 01:20:02 +0530

    feat:HOME

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ const App = () => {
         <Route path="/appointment" element={<MyAppointment/>} />
       </Routes>
       
+      
     </div>
     
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctor from '../components/TopDoctor'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+      <SpecialityMenu/>
+      <TopDoctor/>
+    </div>
   )
 }
 
